80b84fbda08279c6103a224addefb9d2387d555d
Mdalia <mdalia@student.21-school.ru>
2025-06-23 19:53:37 +0700

    create models

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,6 +1,49 @@
-from sqlalchemy import Column, String, UUID
+import uuid
+from enum import Enum as PyEnum
+from sqlalchemy import (
+    Column,
+    String,
+    UUID,
+    Integer,
+    Numeric,
+    Enum,
+    ForeignKey
+)
+from sqlalchemy.orm import relationship
 
 from src.database import Base
 
+class UserRole(PyEnum):
+    USER = "user"
+    ADMIN = "admin"
+
 class User(Base):
-    pass
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, unique=True)
+    password = Column(String, nullable=False)
+    full_name= Column(String, nullable=False)
+    role = Column(Enum(UserRole), nullable=False, default=UserRole.USER)
+
+    accounts = relationship("Account", back_populates="user")
+    transactions = relationship("Transaction", back_populates="user")
+
+class Account(Base):
+    __tablename__ = "accounts"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    balance = Column(Numeric, nullable=False, default=0.0)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    user = relationship("User", back_populates="accounts")
+    transactions = relationship("Transaction", back_populates="account")
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    account_id = Column(Integer, ForeignKey("accounts.id"), nullable=False)
+    amount = Column(Numeric, nullable=False)
+
+    user = relationship("User", back_populates="transactions")
+    account = relationship("Account", back_populates="transactions")
